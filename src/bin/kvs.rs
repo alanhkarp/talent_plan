a88961b7004762acf2ddd5f3df0284d5d6d9fe5f
alanhkarp <alanhkarp@gmail.com>
2019-06-18 16:21:18 -0700

    Use StructOpt instead of using clap directly

--- src/bin/kvs.rs
+++ src/bin/kvs.rs
@@ -1,49 +1,51 @@
 
 use std::process::exit;
 
+use structopt::StructOpt;
+
 use clap::{App, Arg, SubCommand};
 
 fn main() {
-    let version = env!("CARGO_PKG_VERSION");
-    let author = env!("CARGO_PKG_AUTHORS");
-    let matches = App::new("Key Value Store")
-        .version(version)
-        .author(author)
-        .about("Pingcap Talent Plan")
-        .subcommand(
-            SubCommand::with_name("get")
-                .help("get key - Get value associated with string key")
-                .arg(Arg::with_name("key").required(true).index(1)),
-        )
-        .subcommand(
-            SubCommand::with_name("set")
-                .help("set key value - Set string value to associate with string key")
-                .arg(Arg::with_name("key").required(true).index(1))
-                .arg(Arg::with_name("value").required(true).index(2)),
-        )
-        .subcommand(
-            SubCommand::with_name("rm")
-                .help("rm key - Get value associated with string key")
-                .arg(Arg::with_name("key").required(true).index(1)),
-        )
-        .get_matches();
-
-    match matches.subcommand() {
-        ("get", Some(_m)) => {
-            eprintln!("unimplemented");
+    let options = CliCmd::from_args();
+    match options.cmd {
+        Command::Get { key } => {
+            eprintln!("unimplemented: get {}", key);
             exit(1);
         }
-        ("set", Some(_m)) => {
-            eprintln!("unimplemented");
+        Command::Set { key, value }=> {
+            eprintln!("unimplemented: set {} {}", key, value);
             exit(1);
         }
-        ("rm", Some(_m)) => {
-            eprintln!("unimplemented");
-            exit(1);
-        }
-        _ => {
-            eprintln!("Must specify a command");
+        Command::Rm { key } => {
+            eprintln!("unimplemented: rm {}", key);
             exit(1);
         }
     }
 }
+#[derive(Debug, StructOpt)]
+struct CliCmd {
+    #[structopt(subcommand)]
+    cmd: Command
+}
+#[derive(Debug, StructOpt)]
+enum Command {
+    #[structopt(name = "get")]
+    Get { 
+        #[structopt(required = true)]
+        key: String
+    },
+    #[structopt(name = "set")]
+    Set {
+        #[structopt(required = true)]
+        key: String,
+        #[structopt(required = true)]
+        value: String
+    },
+    #[structopt(name = "rm")]
+    Rm {
+        #[structopt(required = true)]
+        key: String
+    }
+}
+
+
